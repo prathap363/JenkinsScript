245ff00ae09903bf196f8142544880a13c8b9c23
Prathap Mohan <prathapmohan@Prathaps-MacBook-Pro.local>
2020-10-06 17:02:23 +0530

    final commit for this test 1

--- testscript.sh
+++ testscript.sh
@@ -6,4 +6,4 @@ echo "hello..$user"
 echo "first Number is: $fno
 echo "last Number is: $lno
 num=$(( $fno + $lno ))
-echo "The sum of two numbers is : $num"
+echo " The sum of two numbers is : $num"
